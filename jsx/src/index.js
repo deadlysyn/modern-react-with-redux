fad1a781a6ef970c8a1c0517bb1d5bc10853768c
Mike Hoskins <mrh@devopsdreams.io>
2020-08-09 11:16:48 -0400

    initial commit

---
+++ jsx/src/index.js
@@ -0,0 +1,24 @@
+// Import react and reactdom libs
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create react component
+const App = () => {
+  const buttonText = { text: "Click me" };
+  const labelText = "Enter name:";
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
+
+// Take react component and show on screen
+ReactDOM.render(<App />, document.getElementById("root"));
