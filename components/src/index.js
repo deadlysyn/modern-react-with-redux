85058ffbf75e50f204270225af3d51158f58e25e
Mike Hoskins <mrh@devopsdreams.io>
2020-08-09 16:24:20 -0400

    add reusable approval component

--- components/src/index.js
+++ components/src/index.js
@@ -3,28 +3,41 @@ import ReactDOM from "react-dom";
 import ComponentDetail from "./CommentDetail";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        avatar={faker.image.avatar()}
-        timeAgo="Today at 4:45PM"
-        content="So informative, thanks!"
-      />
-      <CommentDetail
-        author="Alex"
-        avatar={faker.image.avatar()}
-        timeAgo="Today at 2:00AM"
-        content="Not the usual echo chamber."
-      />
-      <CommentDetail
-        author="Jane"
-        avatar={faker.image.avatar()}
-        timeAgo="Yesterday at 5:00PM"
-        content="Hope you're having fun with props!"
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          avatar={faker.image.avatar()}
+          timeAgo="Today at 4:45PM"
+          content="So informative, thanks!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          avatar={faker.image.avatar()}
+          timeAgo="Today at 2:00AM"
+          content="Not the usual echo chamber."
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          avatar={faker.image.avatar()}
+          timeAgo="Yesterday at 5:00PM"
+          content="Hope you're having fun with props!"
+        />
+      </ApprovalCard>
     </div>
   );
 };
