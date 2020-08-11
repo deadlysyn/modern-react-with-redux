8aba0fa2866b15bddd1aefe869abcf1d5909889d
Mike Hoskins <mrh@devopsdreams.io>
2020-08-10 23:07:31 -0400

    factor conditionals out of render statement

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -17,7 +17,7 @@ class App extends React.Component {
     console.log("Component updated");
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -25,6 +25,10 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept location request..." />;
+  }
+
+  render() {
+    return <div className="border">{this.renderContent()}</div>;
   }
 }
 
