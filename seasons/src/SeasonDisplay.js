49016b4119ed0b5bc224b0c95042bb86c4a264e8
Mike Hoskins <mrh@devopsdreams.io>
2020-08-10 22:12:23 -0400

    factor out config object

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Brr, it's chilly.",
+    iconName: "snowflake",
+  },
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -9,14 +20,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === "winter" ? "Brr it's chilly" : "Let's hit the beach";
-  const icon = season === "winter" ? "snowflake" : "sun";
+  const { text, iconName } = seasonConfig[season];
 
   return (
     <div>
-      <i className={`${icon} icon big`} />
+      <i className={`${iconName} icon big`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon big`} />
+      <i className={`${iconName} icon big`} />
     </div>
   );
 };
