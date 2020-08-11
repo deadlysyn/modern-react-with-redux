c60048247623db8d36d35fa087c122198e98ce2d
Mike Hoskins <mrh@devopsdreams.io>
2020-08-10 22:09:27 -0400

    util method to determine season; add app text and icons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,24 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  }
+  return lat > 0 ? "winter" : "summer";
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === "winter" ? "Brr it's chilly" : "Let's hit the beach";
+  const icon = season === "winter" ? "snowflake" : "sun";
+
+  return (
+    <div>
+      <i className={`${icon} icon big`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon big`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
