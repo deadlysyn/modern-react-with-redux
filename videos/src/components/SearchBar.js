955ea868fef8e0e51ee9539a1064c4b666ba7ef8
Mike Hoskins <mrh@devopsdreams.io>
2020-08-16 21:54:48 -0400

    wire up search bar to youtube api

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -9,7 +9,7 @@ class SearchBar extends React.Component {
 
   onFormSubmit = (event) => {
     event.preventDefault();
-    console.log("Submitted");
+    this.props.onFormSubmit(this.state.term);
   };
 
   render() {
