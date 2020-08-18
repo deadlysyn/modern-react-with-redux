6cd526d1d88f5a27bb143e13c4050ad43631df2a
Mike Hoskins <mrh@devopsdreams.io>
2020-08-17 22:45:34 -0400

    expand item on click

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,18 +1,21 @@
-import React from "react";
-
-const onTitleClick = (index) => {
-  console.log("title clicked", index);
-};
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClick(index)}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
