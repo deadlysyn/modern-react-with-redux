67a5863e358a05496c8333fd3e1956cc6e896220
Mike Hoskins <mrh@devopsdreams.io>
2020-08-18 22:56:41 -0400

    render search results list

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -21,6 +21,17 @@ const Search = () => {
     if (term) search();
   }, [term]);
 
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="ui form">
@@ -33,6 +44,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
