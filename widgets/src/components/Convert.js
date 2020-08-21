f4bdaacb19a2ad88d256798a6a4d38f3b2b72322
Mike Hoskins <mrh@devopsdreams.io>
2020-08-20 23:22:11 -0400

    initial commit

---
+++ widgets/src/components/Convert.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_TRANSLATE_API_KEY;
+
+const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: API_KEY,
+          },
+        }
+      );
+
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
+  }, [language, text]);
+
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
+};
+
+export default Convert;
